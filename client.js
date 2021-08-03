0acddaa650827f4dd36c95285c2de3dfe726358f
unsigned_char <77413577+lvlou@users.noreply.github.com>
2021-08-03 11:31:14 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 console.log("hi");
 
+var WAIT = (ms) => new Promise(res => setTimeout(res, ms));
+var Properties = null;
 var PropertiesReceived = false;
+
+CheckPos();
 
 on("playerSpawned", () => {
     emitNet("Properties->RequestList");
@@ -14,6 +18,7 @@ on("onResourceStart", (res) => {
 });
 
 onNet("Properties->ReceiveList", (properties) => {
+    Properties = properties;
     PropertiesReceived = false;
     console.log(properties);
 
@@ -40,3 +45,36 @@ onNet("Properties->ReceiveList", (properties) => {
         console.log(`creating blip for ${property.garageName}`);
     }
 });
+
+async function CheckPos()
+{
+    if (PropertiesReceived)
+    {
+        while (true)
+        {
+            var localPos = GetEntityCoords(PlayerPedId());
+            for (property of Properties)
+            {
+                var d = GetDistance(localPos[0], localPos[1], localPos[2], property.extCoords[0], property.extCoords[1], property.extCoords[2]);
+
+                if (d < 20)
+                {
+                    console.log(`player is closest to ${property.name} at ${property.extCoords}`);
+                }
+            }
+
+            await WAIT(5000);
+        }
+    }
+}
+
+function GetDistance(x1, y1, z1, x2, y2, z2)
+{
+    var a = x2 - x1;
+    var b = y2 - y1;
+    var c = z2 - z1;
+
+    var distance = Math.sqrt((a * a) + (b * b) + (c * c));
+
+    return distance;
+}
