fa2e2f92c83a0021eb9e4c970da56f2df9029fe7
unsigned_char <77413577+lvlou@users.noreply.github.com>
2021-08-03 14:02:56 +0100

    Update client.js

--- client.js
+++ client.js
@@ -77,7 +77,7 @@ async function CheckPos()
                     InRange = false;
                     DrawingMarkers = false;
                     ClosestProperty = null;
-                    Enter_Called = true;
+                    Enter_Called = false;
                     
                     if (!Exit_Called)
                     {
