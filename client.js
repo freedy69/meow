08428bb68dec909b7281bb862b668c3ac267fb4b
unsigned_char <77413577+lvlou@users.noreply.github.com>
2021-08-03 11:18:34 +0100

    Update client.js

--- client.js
+++ client.js
@@ -20,14 +20,23 @@ onNet("Properties->ReceiveList", (properties) => {
     for (property of properties)
     {
         AddTextEntry(property.txtEntry, property.name);
+        AddTextEntry(property.garageTxtEntry, property.garageName);
 
         var blip = AddBlipForCoord(property.extCoords[0], property.extCoords[1], property.extCoords[2]);
         SetBlipSprite(blip, property.blipId);
-        SetBlipCategory(blip, 10);
         SetBlipAsShortRange(blip, true);
+        SetBlipCategory(blip, 10);
         BeginTextCommandSetBlipName(property.txtEntry);
         EndTextCommandSetBlipName(blip);
 
-        console.log(`creating blip for ${property}`);
+        console.log(`creating blip for ${property.name}`);
+
+        var garageBlip = AddBlipForCoord(property.garageCoords[0], property.garageCoords[1], property.garageCoords[2]);
+        SetBlipSprite(garageBlip, property.garageBlipId);
+        SetBlipAsShortRange(garageBlip, true);
+        BeginTextCommandSetBlipName(property.garageTxtEntry);
+        EndTextCommandSetBlipName(garageBlip);
+
+        console.log(`creating blip for ${property.garageName}`);
     }
 });
