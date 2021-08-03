699cfeb217999390da4c6feae0d9b08048afc1be
unsigned_char <77413577+lvlou@users.noreply.github.com>
2021-08-03 10:37:02 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 console.log("hi");
+
+console.log("hi");
+
+on("playerSpawned", () => {
+    emitNet("Properties->RequestList");
+});
+
+on("onResourceStart", (res) => {
+    if (res == GetCurrentResourceName())
+    {
+        emitNet("Properties->RequestList");
+    }
+});
+
+onNet("Properties->ReceiveList", (properties) => {
+    console.log(properties);
+});
