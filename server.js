157b400f1fb2de0cd8990924c1b77f5d271029df
unsigned_char <77413577+lvlou@users.noreply.github.com>
2021-08-03 10:27:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log("hi");
+
+var Properties =
+{
+    EclipseTwr:
+    {
+        extCoords: [-774.02, 310.89, 85.7],
+        name: "Eclipse Tower Apartments"
+    }
+};
+
+console.log(Properties);
