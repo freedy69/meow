c0d9c6c59754882ce735851af985461d71fa0e97
unsigned_char <77413577+lvlou@users.noreply.github.com>
2021-08-09 16:20:14 +0100

    add images to files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,5 +2,5 @@ fx_version "cerulean"
 game "gta5"
 server_script "src/server/server.js"
 client_script "src/client/client.js"
-files { 'html/**', 'css/**', 'fonts/**' }
+files { 'html/**', 'css/**', 'fonts/**', 'img/** }
 ui_page 'html/index.html'
